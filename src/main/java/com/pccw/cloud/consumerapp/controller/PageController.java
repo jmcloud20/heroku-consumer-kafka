e0f5de7d1367ed76e708b641d7cbecc61c4886ca
josephg <jmgarcia214@yahoo.com>
2020-07-27 12:40:30 +0800

    * Add clear context to avoid over clogging of page during load testing.

--- src/main/java/com/pccw/cloud/consumerapp/controller/PageController.java
+++ src/main/java/com/pccw/cloud/consumerapp/controller/PageController.java
@@ -18,9 +18,23 @@ public class PageController {
 
     @GetMapping("/")
     public String index(Model model) {
+        this.setModel(model);
+        return "index";
+    }
+
+    @GetMapping("/clear")
+    public String clearContext(Model model) {
+        servletContext.removeAttribute(KafkaCloudListener.CUSTOMER_OPT);
+        servletContext.removeAttribute(KafkaCloudListener.UPDATE_EMAIL);
+        servletContext.removeAttribute(KafkaCloudListener.PROD_OFFER);
+        this.setModel(model);
+        return "index";
+    }
+
+    private void setModel(Model model) {
+
         model.addAttribute(KafkaCloudListener.CUSTOMER_OPT, servletContext.getAttribute(KafkaCloudListener.CUSTOMER_OPT));
         model.addAttribute(KafkaCloudListener.UPDATE_EMAIL, servletContext.getAttribute(KafkaCloudListener.UPDATE_EMAIL));
         model.addAttribute(KafkaCloudListener.PROD_OFFER, servletContext.getAttribute(KafkaCloudListener.PROD_OFFER));
-        return "index";
     }
 }
